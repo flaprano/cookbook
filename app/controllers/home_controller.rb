cc6e0d7924f0b8cd2ef2c8a8f97bb0c0a6484b68
Felipe Laprano <flaprano@gmail.com>
2017-06-24 19:08:48 -0300

    Visitante filtra receitas por cozinha

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @cuisines = Cuisine.all
     @recipes = Recipe.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   resources :recipes, only: [:create, :new, :show]
+  resources :cuisines, only: [:show]
 end
