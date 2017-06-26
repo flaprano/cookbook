46b6dca31f14dd12ae96f1958728cd12c5049142
Felipe Laprano <flaprano@gmail.com>
2017-06-26 19:49:48 -0300

    Correções

--- db/migrate/20170624222423_remove_recipetype_from_recipes.rb
+++ db/migrate/20170624222423_remove_recipetype_from_recipes.rb
@@ -1,6 +1,5 @@
 class RemoveRecipetypeFromRecipes < ActiveRecord::Migration[5.1]
   def change
     remove_column :recipes, :recipe_type, :string
-    remove_column :recipes, :recype_type, :string
   end
 end
